b2bf245b699bc8f04abb15a8d4080e25fbf6a9c5
Tyler Baker <tylerhbaker99@gmail.com>
2022-05-29 18:57:57 -0400

    fixed version number

--- src/MangaGohan/MangaGohan.ts
+++ src/MangaGohan/MangaGohan.ts
@@ -39,7 +39,7 @@ const headers = {
 const method = 'GET'
 
 export const MangaGohanInfo: SourceInfo = {
-  version: '1.0.1',
+  version: '1.0.2',
   name: 'Manga Gohan',
   icon: 'logo.png',
   author: 'btylerh7',
